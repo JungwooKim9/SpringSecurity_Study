653195649b31db31f0274b9c5abfbcbbabdb96a1
Kimjungwoo <rara980928@gmail.com>
2023-09-21 22:58:43 +0900

    Changes

--- src/main/java/com/example/jung9k_shop/configuration/SecurityConfig.java
+++ src/main/java/com/example/jung9k_shop/configuration/SecurityConfig.java
@@ -1,13 +1,28 @@
 package com.example.jung9k_shop.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.jung9k_shop.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserService userService;
+
+    @Value("${jwt.secret}")
+    private String key;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -16,12 +31,14 @@ public class SecurityConfig {
                 .csrf().disable()
                 .cors().and()
                 .authorizeRequests()
-                .antMatchers("/api/**").permitAll()
-                .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
+                // .antMatchers("/api/**").permitAll()
+                .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll() // Join, Login은 언제나 가능
+             .antMatchers(HttpMethod.POST, "api/v1/reviews").authenticated()
                 .and()
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt 사용하는 경우 씀
                 .and()
+            // .addFilterBefore(new JwtFilter(userService, key), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
